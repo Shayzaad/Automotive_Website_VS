956a0c2cf557a327b77bb449f92ee50b0a4189b3
Shayzaad <112894216+Shayzaad@users.noreply.github.com>
2024-10-04 19:58:50 +0200

    Revert "Added 75% functioning home page and products page"
    This reverts commit 1e9bc5528dd50cf2107ec4e1a87d5c0cdbb4c068.

--- Front_End/Index.aspx.designer.cs
+++ Front_End/Index.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace Front_End
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl DisProd;
+
+        /// <summary>
+        /// DiscountProd control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl DiscountProd;
     }
 }
